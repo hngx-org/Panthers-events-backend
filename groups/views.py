cb5dd4ca5bff604e25d44397a3ec8f532126dbd9
Nazzcodek <bellnas09@gmail.com>
2023-09-19 21:16:29 +0100

    [feature] groups, user_group and event_group

---
+++ groups/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from .models import Group, UserGroups, GroupEvents
+from .serializers import GroupSerializer, UserGroupsSerializer, GroupEventsSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class UserGroupsViewSet(viewsets.ModelViewSet):
+    queryset = UserGroups.objects.all()
+    serializer_class = UserGroupsSerializer
+
+class GroupEventsViewSet(viewsets.ModelViewSet):
+    queryset = GroupEvents.objects.all()
+    serializer_class = GroupEventsSerializer
