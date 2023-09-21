913d6a0da592a3d0c0a53394961797a4d3e4ce4b
TessyJames28 <tessyjames28@gmail.com>
2023-09-21 12:25:36 +0100

    Get user group details

--- groups/urls.py
+++ groups/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GroupViewSet, UserGroupsViewSet, GroupEventsViewSet, GroupUserView
-
+from .views import GroupViewSet, UserGroupsViewSet, GroupEventsViewSet
 router = DefaultRouter()
 router.register(r'groups', GroupViewSet)
+router.register(r'groups/<str:id>', GroupViewSet)
 router.register(r'usergroups', UserGroupsViewSet)
 router.register(r'groupevents', GroupEventsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
