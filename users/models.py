bfbcc450434c9e8c3a9861578987b3481dd8e228
TessyJames28 <tessyjames28@gmail.com>
2023-09-21 11:43:59 +0100

    user model improvement

--- users/models.py
+++ users/models.py
@@ -1,49 +1,22 @@
 from django.db import models
-from PIL import Image
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
-
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
-        if not email:
-            raise ValueError(_('The Email field must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, username, password, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    avatar = models.ImageField(upload_to='images/', blank=True)
-    password = models.CharField(max_length=128)  # Store hashed password
-
+class User(models.Model):
+    id = models.CharField(max_length=60, primary_key=True, editable=False)
+    name = models.CharField(max_length=255 ,blank=True)
+    email = models.CharField(max_length=255, unique=True)
+    avatar = models.CharField(max_length=255,blank=True)
     
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ['name', 'email', 'avatar']
+    
     def __str__(self):
-        return self.email
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.avatar.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        return self.name
+    
+    @property
+    def is_anonymous(self):
+        return False
+    
+    @property
+    def is_authenticated(self):
+        return True
