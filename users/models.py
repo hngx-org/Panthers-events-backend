ae2e75bb71724e63af5e43a7c0ec9d6919401569
TessyJames28 <tessyjames28@gmail.com>
2023-09-21 22:48:55 +0100

    Improved the models per database schema

--- users/models.py
+++ users/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+import uuid
 
 class User(models.Model):
-    id = models.CharField(max_length=60, primary_key=True, editable=False)
-    name = models.CharField(max_length=255 ,blank=True)
+    id = models.CharField(max_length= 60, primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
-    avatar = models.CharField(max_length=255,blank=True)
+    avatar = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     
     USERNAME_FIELD = 'id'
-    REQUIRED_FIELDS = ['name', 'email', 'avatar']
+    REQUIRED_FIELDS = ['name', 'email', 'avatar', 'created_at', 'updated_at']
     
     def __str__(self):
         return self.name
