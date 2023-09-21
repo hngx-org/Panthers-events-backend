9b8cb61fc1692865aaf7d195a8ae75c55ac0a4b1
John Diginee <Johndbizz@gmail.com>
2023-09-21 23:56:35 +0100

    Chore: Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -163,7 +163,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
