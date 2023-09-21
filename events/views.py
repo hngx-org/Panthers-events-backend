b8b5130ce42d4ee968a4091b046b32b9e5824171
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-09-21 19:01:07 +0100

    Update-Delete event details and Create event comments

--- events/views.py
+++ events/views.py
@@ -79,7 +79,7 @@ class ExpressInterestView(CreateAPIView):
         userId = self.kwargs.get('userId')
         eventId = self.kwargs.get('eventId')
 
-        #Validate if user and event exist
+        # Validate if user and event exist
 
         try:
             user = User.objects.get(id=userId)
@@ -89,12 +89,36 @@ class ExpressInterestView(CreateAPIView):
         except Event.DoesNotExist:
             return Response({"message": "Event does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
-        #creates association between user and event
+        # creates association between user and event
         user.interested_events.add(event)
         user.save()
 
         return Response({"message": "Interest expressed successfully"}, status=status.HTTP_201_CREATED)
-    
+
+
+class DeleteExpressInterestView(DestroyAPIView):
+    serializer_class = EventSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        userId = self.kwargs.get('userId')
+        eventId = self.kwargs.get('eventId')
+
+        # Validate if user and event exist
+
+        try:
+            user = User.objects.get(id=userId)
+            event = Event.objects.get(id=eventId)
+        except User.DoesNotExist:
+            return Response(data={"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        except Event.DoesNotExist:
+            return Response(data={"message": "Event does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        # deletes association between user and event
+        user.interested_events.delete(event)
+
+        return Response(data={"message": "Interest deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
 # ADDING IMAGE TO A COMMENT(POST)
 class CreateImage(viewsets.ModelViewSet):
     queryset = Image.objects.all()
@@ -105,13 +129,14 @@ class CreateImage(viewsets.ModelViewSet):
             comment = Comment.objects.get(id=commentId)
         except Comment.DoesNotExist:
             return Response({"error": "Comment not Found."}, status=status.HTTP_404_NOT_FOUND)
-        
+
         imageData = request.data.get('image')
         if imageData:
             Image.objects.create(comment=comment, image=imageData)
             return Response({"message": "Image added to the comment successfully."}, status=status.HTTP_201_CREATED)
         else:
             return Response({"error": "Image data is missing."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 # GETTING IMAGES FOR A COMMENT(GET)
 class ListImage(viewsets.ModelViewSet):
@@ -123,7 +148,8 @@ class ListImage(viewsets.ModelViewSet):
             comment = Comment.objects.get(id=commentId)
         except Comment.DoesNotExist:
             return Response({"error": "Comment not found."}, status=status.HTTP_404_NOT_FOUND)
-        
+
         images = Image.objects.filter(comment=comment)
         serializer = ImageSerializer(images, many=True)
         return Response(serializer.data)
+    
