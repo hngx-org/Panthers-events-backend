c144fcd53a7027c4820b9807ec5afedc76c8058a
Nazzcodek <bellnas09@gmail.com>
2023-09-21 11:16:38 +0100

    [feature] added the Image and GroupImage model

--- groups/serializers.py
+++ groups/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from .models import Group, UserGroups, GroupEvents
+from .models import Image, Group, UserGroups, GroupEvents, GroupImage
 from users.models import User
 from events.models import Event
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
 
 class UserGroupsSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
@@ -16,10 +24,17 @@ class UserGroupsSerializer(serializers.ModelSerializer):
         model = UserGroups
         fields = '__all__'
 
+
 class GroupEventsSerializer(serializers.ModelSerializer):
     event = serializers.PrimaryKeyRelatedField(queryset=Event.objects.all())
     group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all())
 
     class Meta:
         model = GroupEvents
+        fields = '__all__'
+
+
+class GroupImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupImage
         fields = '__all__'
--- groups/views.py
+++ groups/views.py
@@ -1,15 +1,31 @@
 from rest_framework import viewsets
-from .models import Group, UserGroups, GroupEvents
-from .serializers import GroupSerializer, UserGroupsSerializer, GroupEventsSerializer
+from .models import Image, Group, UserGroups, GroupEvents, GroupImage
+from .serializers import (ImageSerializer,
+                          GroupSerializer,
+                          UserGroupsSerializer,
+                          GroupEventsSerializer,
+                          GroupImageSerializer)
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+
 class UserGroupsViewSet(viewsets.ModelViewSet):
     queryset = UserGroups.objects.all()
     serializer_class = UserGroupsSerializer
 
+
 class GroupEventsViewSet(viewsets.ModelViewSet):
     queryset = GroupEvents.objects.all()
     serializer_class = GroupEventsSerializer
+
+
+class GroupImageViewSet(viewsets.ModelViewSet):
+    queryset = GroupImage.objects.all()
+    serializer_class = GroupImageSerializer
