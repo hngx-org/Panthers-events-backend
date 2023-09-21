05b2cd4c3a1a7adfd8ae8002214006be32e1d92e
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-09-21 14:30:33 +0100

    Update/Delete Event details and Create Event comments

--- events/urls.py
+++ events/urls.py
@@ -6,11 +6,13 @@ from . views import CreateImage, ListImage
 urlpatterns = [
     path('api/events/', views.EventListAPIView.as_view(), name='event-list'),
     path('api/events/', views.CreateEventAPIView.as_view(), name='event-create'),
-
     path('api/events/<int:pk>/', views.RetrieveEventAPIView.as_view(), name='event-retrieve'),
+
+    path('api/events/<int:eventId>/', views.PutDeleteEventDetail.as_view(), name='event_detail'),
+    path('api/events/<int:eventId>/comments/', views.PostEventComment.as_view(), name='event_comment'),
 
     path('api/users/<int:userId>/interests/<int:eventId>/', views.ExpressInterestView.as_view(),
          name='express-interest'),
     path('api/comments/<int:commentId>/images/', CreateImage.as_view({'post': 'create'}), name='add-comment-image'),
-    path('api/comments/<int:commentId>/images/list/', ListImage.as_view({'get': 'list'}), name='list-comment-images')
+    path('api/comments/<int:commentId>/images/list/', ListImage.as_view({'get': 'list'}), name='list-comment-images'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,11 +1,13 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
-from events.models import Event
-from events.serializers import EventSerializer, ExpressInterestSerializer
+from events.models import Event, Comment
+from events.serializers import EventSerializer, ExpressInterestSerializer, CommentSerializer
 from users.models import User
 from rest_framework import status, viewsets
 from .models import Image, Comment
 from .serializers import ImageSerializer
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 
 class EventListAPIView(ListAPIView):
@@ -21,6 +23,53 @@ class CreateEventAPIView(CreateAPIView):
 class RetrieveEventAPIView(RetrieveAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+# Verify the event object from the Event model
+def get_event_object(eventId):
+    if not isinstance(eventId, int):
+        return Response({"error": "Event ID must be an integer format."}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        event = get_object_or_404(Event, id=eventId)
+        return event
+
+
+class PutDeleteEventDetail(APIView):
+    """Update or Delete an event detail."""
+    def put(self, request, eventId):
+        event = get_event_object(eventId=eventId)
+
+        # Ensure only the event creator can UPDATE the event details
+        if request.user == event.creator:
+            serializer = EventSerializer(instance=event, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'You don\'t have permission to make this request.'},
+                            status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request, eventId):
+        event = get_event_object(eventId=eventId)
+        # Ensure only the event creator can DELETE the event
+        if request.user == event.creator:
+            event.delete()
+            return Response({'message': 'Event successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({'error': 'You don\'t have permission to make this request.'},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class PostEventComment(APIView):
+    """Create a comment for an event."""
+    def post(self, request, eventId):
+        event = get_event_object(eventId=eventId)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(event=event, creator=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ExpressInterestView(CreateAPIView):
