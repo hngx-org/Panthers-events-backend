58060c7c7f05dd29b3e9034ccde7bf432dda8c4c
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-21 09:06:14 +0100

    user interaction 1

--- events/urls.py
+++ events/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path('api/events/', views.EventListAPIView.as_view(), name='event-list'),
-    path('api/events/', views.EventCreateAPIView.as_view(), name='event-create'),
+    path('api/events/', views.CreateEventAPIView.as_view(), name='event-create'),
     path('api/events/<int:pk>/', views.EventRetrieveAPIView.as_view(), name='event-retrieve'),
+    path('api/users/<int:userId>/interests/<int:eventId>/', views.ExpressInterestView.as_view(),
+         name='express-interest'),
 ]
-
