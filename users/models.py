538e9f052a8699d2e75989c391f5336dc5fbbe13
Nazzcodek <bellnas09@gmail.com>
2023-09-21 18:26:37 +0100

    [feature] updated the user model

--- users/models.py
+++ users/models.py
@@ -1,11 +1,37 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class User(models.Model):
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.CharField(max_length=60, primary_key=True, editable=False)
-    name = models.CharField(max_length=255 ,blank=True)
-    email = models.CharField(max_length=255, unique=True)
-    avatar = models.CharField(max_length=255,blank=True)
-    
-    USERNAME_FIELD = 'id'
-    REQUIRED_FIELDS = ['name', 'email', 'avatar']
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, blank=True)
+    avatar = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'avatar']
+
+    def __str__(self):
+        return self.email
