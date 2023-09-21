3be81dc0a5e3f7e5f1c8454229eccd782f331b28
Konlan21 <joekonlan16@gmail.com>
2023-09-21 18:37:15 +0000

    Combine list and create event operations into a single view

--- events/urls.py
+++ events/urls.py
@@ -4,9 +4,7 @@ from . views import CreateImage, ListImage
 
 
 urlpatterns = [
-    path('api/events/', views.EventListAPIView.as_view(), name='event-list'),
-    path('api/events/', views.CreateEventAPIView.as_view(), name='event-create'),
-
+    path('api/events/', views.EventListAPIView.as_view(), name='event-list-create'),
     path('api/events/<int:pk>/', views.RetrieveEventAPIView.as_view(), name='event-retrieve'),
 
     path('api/users/<int:userId>/interests/<int:eventId>/', views.ExpressInterestView.as_view(),
