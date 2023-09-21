0064b50da7aaf9604620f98a8725402e558e21c4
Konlan21 <joekonlan16@gmail.com>
2023-09-21 09:34:37 +0000

    Fix class name mismatch in url file

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('api/events/', views.EventListAPIView.as_view(), name='event-list'),
-    path('api/events/', views.EventCreateAPIView.as_view(), name='event-create'),
-    path('api/events/<int:pk>/', views.EventRetrieveAPIView.as_view(), name='event-retrieve'),
+    path('api/events/', views.CreateEventAPIView.as_view(), name='event-create'),
+    path('api/events/<int:pk>/', views.RetrieveEventAPIView.as_view(), name='event-retrieve'),
 ]
 
