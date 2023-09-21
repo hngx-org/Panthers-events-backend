dc28e1d2719f777812ee92cbcfcd3c93c5e71340
Nazzcodek <bellnas09@gmail.com>
2023-09-21 23:23:34 +0100

    added created_at, updated_at and creator_id fields

--- groups/models.py
+++ groups/models.py
@@ -11,7 +11,10 @@ class Image(models.Model):
 class Group(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=225)
-
+    creator_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.title
 
