106fa021b126c0cac4e15fff7b1cf865775c6388
John Diginee <Johndbizz@gmail.com>
2023-09-22 02:21:46 +0100

    Chore: Fix bug

--- api/settings.py
+++ api/settings.py
@@ -111,15 +111,11 @@ if IS_HEROKU_APP:
     # https://github.com/jazzband/dj-database-url
     
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
-    }
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
 
 else:
