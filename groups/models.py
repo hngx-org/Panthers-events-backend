d41260693f547ebce7502ce7f4578a49c6268258
Nazzcodek <bellnas09@gmail.com>
2023-09-23 17:13:03 +0100

    updated

--- groups/models.py
+++ groups/models.py
@@ -8,30 +8,29 @@ class Image(models.Model):
                           primary_key=True,
                           editable=False,
                           default=generateUUID,
-                          db_column='id')
-    url = models.TextField(db_column='url')
-    created_at = models.DateTimeField(auto_now_add=True, db_column='created_at')
-    updated_at = models.DateTimeField(auto_now=True, db_column='updated_at')
+                         )
+    url = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class meta:
+        managed = False
         db_table = 'images'
 
-  
+
 class Group(models.Model):
     id = models.CharField(max_length=255,
                           primary_key=True,
                           editable=False,
                           default=generateUUID,
-                          db_column='id')
+                          )
     title = models.CharField(max_length=225)
-    creator_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='creator_id')
-    created_at = models.DateTimeField(auto_now_add=True, db_column='created_at')
-    updated_at = models.DateTimeField(auto_now=True, db_column='updated_at')
-    
-    def __str__(self):
-        return self.title
+    creator_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
  
     class meta:
+        managed = False
         db_table = 'groups'
 
 
@@ -39,31 +38,27 @@ class UserGroups(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
     group = models.ForeignKey(Group, on_delete=models.CASCADE, db_column='group_id')
 
-    def __str__(self):
-        return f"User ID: {self.user}, Group ID: {self.group}"
-
     class Meta:
+        managed = False
         db_table = 'user_groups'
+        unique_together = (('user', 'group'),)
 
 
 class GroupEvents(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, db_column='event_id')
     group = models.ForeignKey(Group, on_delete=models.CASCADE, db_column='group_id')
 
-    def __str__(self):
-        return f"Event ID: {self.event}, Group ID: {self.group}"
- 
     class Meta:
+        managed = False
         db_table = 'group_events'
+        unique_together = (('group', 'event'),)
 
 
 class GroupImage(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, db_column='group_id')
     image = models.ForeignKey(Image, on_delete=models.CASCADE, db_column='image_id')
 
-    def __str__(self):
-        return f"Group Name: {self.group.title}, Image URL: {self.image.url}"
-
     class Meta:
+        managed = False
         db_table = 'group_images'
-
+        unique_together = (('group', 'image'),)
